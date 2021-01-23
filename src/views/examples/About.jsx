f1318c6bcd7d62b606c8b01695e2914805459fd3
Paulo Teixeira <hst.paulo@gmail.com>
2021-01-23 13:35:17 -0300

    finalizando modulo de navegacao

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = props => (
+    <div className="About">
+        <h1>Sobre</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+    </div>
+)
+
+export default About
